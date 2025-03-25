354caf988a31df76da06ec55a82163967dfb6fee
Viktor-Kizera <victorkizera2003@gmail.com>
2025-03-25 10:15:14 +0200

    Initial Commit

---
+++ SmartStock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SmartStock
+//
+//  Created by Viktor Kizera on 3/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SmartStock/SmartStockApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartStockApp.swift
+//  SmartStock
+//
+//  Created by Viktor Kizera on 3/25/25.
+//
+
+import SwiftUI
+
+@main
+struct SmartStockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
